bb1368eea7a36609129b9061cf166b155af0902b
HUDAAYOUB <huda3ya@gmail.com>
2023-09-14 18:23:51 +0300

    Q3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int number = 6;
+
+
+        if (number % 2 == 0)
+        {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }}}
+
